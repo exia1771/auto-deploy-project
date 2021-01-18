e57a6b951aa8d710e9f44998d30546193f0a4911
xuwenwu <xuwenwu@kedacom.com>
2021-01-18 17:01:25 +0800

    Title: 【初始化工程】 Author: exia1771 Content: 工程文件初始化 Datetime: 2021-01-18 17:01

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,14 +1,46 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true
   },
-  extends: ["plugin:vue/essential", "eslint:recommended", "@vue/prettier"],
+  extends: ["plugin:vue/essential", "eslint:recommended"],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: "babel-eslint",
+    "sourceType": "module"
   },
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off"
+  // required to lint *.vue files
+  plugins: [
+    'html'
+  ],
+  // add your custom rules here
+  'rules': {
+    // 单引号引用字符串
+    'quotes': [
+      'error',
+      'single'
+    ],
+    // 必须使用分号结束
+    'semi': [
+      'error',
+      'always'
+    ],
+    'space-before-function-paren': 0,
+    'spaced-comment': 0,
+    // 允许tab缩进
+    'no-tabs': 'off',
+    // 关闭===检测
+    'eqeqeq': 'off',
+    // 允许多行空行
+    'no-multiple-empty-lines': 'off',
+    // 允许多余空格
+    'no-trailing-spaces': 'off',
+    // end of file last line
+    'eol-last': 'off',
+    // promise reject不需要error对象
+    'prefer-promise-reject-errors': 'off',
+    'keyword-spacing': 0,
+    'space-before-blocks': 0,
+    // allow debugger during development
+    'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0
   }
 };
