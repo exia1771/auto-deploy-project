e15d9be68e7c85a00f53a3ebb71451ec659afbc6
exia1771 <exia1771@gmail.com>
2021-01-21 15:52:04 +0800

    Title: 【初始化工程】 Author: exia1771 Content: 工程文件初始化 Datetime: 2021-01-21 15:51

---
+++ config/global.js
@@ -0,0 +1,3 @@
+const ROOT_SERVER_URL = "localhost:8080";
+
+export default Global;
---
+++ src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Hello This is Login Page</div>
+</template>
+
+
+<script>
+export default {
+  name: 'Login'
+};
+</script>
+
+
+<style scoped>
+</style>
